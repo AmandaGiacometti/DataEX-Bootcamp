912d53d2b3ecb11635513c6874c60bc98cf7ff6b
Amanda Giacometti <amandagiacomettipedrosa@gmail.com>
2023-02-17 16:53:17 -0300

    Adicionando aula 06

---
+++ Dia 06/CRIA_STG.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [dbo].[STG_VENDA](
+    [SEGMENTO] [varchar](50) NULL,
+    [PAIS] [varchar](50) NULL,
+    [PRODUTO] [varchar](50) NULL,
+    [TIPO_DESCONTO] [varchar](50) NULL,
+    [QTDE_VENDAS] [varchar](50) NULL,
+    [PRECO_CUSTO] [varchar](50) NULL,
+    [PRECO_VENDA] [varchar](50) NULL,
+    [VENDAS_BRUTA] [varchar](50) NULL,
+    [DESCONTO] [varchar](50) NULL,
+    [VENDAS] [varchar](50) NULL,
+    [CUSTO_VENDA] [varchar](50) NULL,
+    [LUCRO] [varchar](50) NULL,
+    [DATA_VENDA] [varchar](50) NULL
+) ON [PRIMARY]
+GO
